2abda2ed8ee9e74a05798fb7347c634896003a0c
Bruno Paulo <bruno@werules.com>
2013-07-10 06:51:43 +0000

    web.js answering from index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var msg = fs.readFile('index.html');
+  var msg = fs.readFileSync('index.html').toString();
   response.send(msg);
 });
 
