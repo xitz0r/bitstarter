f6888e32b446388fec44315166875598d5f40031
Bruno Paulo <bruno@werules.com>
2013-07-10 06:40:51 +0000

    Changed web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var msg = fs.readFile('index.html').toString();
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
